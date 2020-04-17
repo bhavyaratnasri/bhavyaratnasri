979dea1a8235b457abc73e23208ac7fab3f5997c
bhavyaratnasri <63100076+bhavyaratnasri@users.noreply.github.com>
2020-04-17 12:43:51 +0530

    Update parseibmstring.cpp

--- case8/parseibmstring.cpp
+++ case8/parseibmstring.cpp
@@ -309,7 +309,7 @@ int main(int argc, char *argv[])
 {
 	if (argc !=2 )
 	{
-		cout<<"Usage PerselIBMString:parserstring.exe  <<inputFileName>>.txt"<<endl;
+		cout<<"Usage PerselIBMString:<<inputFileName>>.txt"<<endl;
 		return -1;
 	}
 	getIBM(argv[1]);
